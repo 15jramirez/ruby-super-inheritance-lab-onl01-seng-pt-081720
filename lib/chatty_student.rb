61c5cd78d7a0217fa50aff2edee088f386718314
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-10-01 00:20:39 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,12 +1,12 @@
 class Chatty_student < Student 
   def hello
     super 
-    "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay, well let me just tell you who died..."
+    puts 
   end
   
   def raise_hand
     super 
-     5.times do 
+     10.times do 
        puts "Pick me!"
      end
    end
